60f6d52a0b23e972308b0a43a786a90ebdfbe6d4
seemonz <seemonz@gmail.com>
2016-01-27 07:51:19 -0800

    add basic header scroll bar functionality.

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -17,3 +17,45 @@ $(function() {
         event.preventDefault();
     });
 });
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+        $('nav').removeClass('nav-down').addClass('nav-fix');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+            $('nav').removeClass('nav-fix').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
